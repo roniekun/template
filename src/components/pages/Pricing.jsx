fec3958438a9216aa203a801b1f4daea992e8844
roniekun <roniebenitez01@gmail.com>
2023-09-27 00:51:44 +0800

    update

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './styles/Pricing.module.css'
 import Header from '../Header'
 import Footer from '../Footer'
+
 const Pricing = ({isDesktop,setShowNavbar,isMediumScreen, isSmallScreen}) => {
   return (
     <div className={styles.container}>
