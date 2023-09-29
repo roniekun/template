0e5eb54878b6f810fcfc872bd50caf917bcf77e8
roniekun <roniebenitez01@gmail.com>
2023-09-30 01:36:04 +0800

    update

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -15,22 +15,31 @@ const About = ({isDesktop, setShowNavbar, isMediumScreen, isSmallScreen,showNavb
               showNavbar={showNavbar}/>
       <div className={styles.body}>
         <div className={styles.sectionOne}>
-          <h1>I&apos;m more than a photographer; I'm a visual storyteller. 
-           </h1>
+
            <h4> Bringing your events to life through the art of storytelling.</h4>
          
            <div className={styles.imageContainer}>
-
+            <div className={styles.about}>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Illo fugiat perspiciatis magni nulla, numquam enim velit cumque aliquam maiores vero excepturi, 
+             delectus obcaecati ducimus magnam pariatur, modi non quas ea. Lorem ipsum dolor sit amet consectetur 
+             adipisicing elit. Aperiam maxime quisquam natus. Quos iure distinctio veniam, 
+             excepturi placeat natus deleniti quas a ratione enim, sunt aspernatur dicta quam praesentium tempore?
              Illo fugiat perspiciatis magni nulla, numquam enim velit cumque aliquam maiores vero excepturi, 
-             delectus obcaecati ducimus magnam pariatur, modi non quas ea. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam maxime quisquam natus. Quos iure distinctio veniam, 
+             delectus obcaecati ducimus magnam pariatur, modi non quas ea. Lorem ipsum dolor sit amet consectetur 
+             adipisicing elit. Aperiam maxime quisquam natus. Quos iure distinctio veniam, 
              excepturi placeat natus deleniti quas a ratione enim, sunt aspernatur dicta quam praesentium tempore?
              </p>
-
-            <img className={styles.image}src="" alt="" />
+             <h1>I&apos;m more than a photographer;<br /> I'm a visual storyteller. 
+              </h1>
+             </div>
+           
+            <img className={styles.image}src="images/headshot.jpg" 
+                        title="Image from Andrew Wise from Unsplash" 
+                        alt="andrew-wise-uQ83_5lc1eQ-unsplash.jpg" />
            </div>
         </div>
-        
+
         <div className={styles.sectionTwo}>
           <h1>Skills</h1>
         </div >
